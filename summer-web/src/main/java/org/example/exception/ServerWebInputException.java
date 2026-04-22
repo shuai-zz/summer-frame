ec5be45a0e9ea96e706f5b2b0a2ced560f239c40
zhaoShuai <s74933347@gmail.com>
2026-04-22 15:29:19 +0800

    feat: MVC Exceptions

---
+++ summer-web/src/main/java/org/example/exception/ServerWebInputException.java
@@ -0,0 +1,22 @@
+package org.example.exception;
+
+/**
+ * @author zhaoshuai
+ */
+public class ServerWebInputException extends ErrorResponseException{
+    public ServerWebInputException() {
+        super(400);
+    }
+
+    public ServerWebInputException(String message) {
+        super(400, message);
+    }
+
+    public ServerWebInputException(Throwable cause) {
+        super(400, cause);
+    }
+
+    public ServerWebInputException(String message, Throwable cause) {
+        super(400, message, cause);
+    }
+}
