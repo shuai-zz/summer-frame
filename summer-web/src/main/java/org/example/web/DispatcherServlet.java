278a7cee8235629877f6cc1eb3aebed1e6c26121
zhaoShuai <s74933347@gmail.com>
2026-04-28 14:18:37 +0800

    fix bug: dispatcher servlet

--- summer-web/src/main/java/org/example/web/DispatcherServlet.java
+++ summer-web/src/main/java/org/example/web/DispatcherServlet.java
@@ -3,15 +3,16 @@ package org.example.web;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.example.annotation.PathVariable;
-import org.example.annotation.RequestBody;
-import org.example.annotation.RequestParam;
-import org.example.annotation.ResponseBody;
+import org.example.annotation.*;
 import org.example.context.ApplicationContext;
+import org.example.context.ConfigurableApplicationContext;
+import org.example.exception.ErrorResponseException;
+import org.example.exception.NestedRuntimeException;
 import org.example.exception.ServerErrorException;
 import org.example.exception.ServerWebInputException;
 import org.example.io.PropertyResolver;
@@ -24,11 +25,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -51,6 +51,32 @@ public class DispatcherServlet extends HttpServlet {
 
     public DispatcherServlet(ApplicationContext applicationContext, PropertyResolver propertyResolver) {
         this.applicationContext = applicationContext;
+        this.viewResolver=applicationContext.getBean(ViewResolver.class);
+        this.resourcePath = propertyResolver.getProperty("${summer.web.static-path:/static/}");
+        this.faviconPath=propertyResolver.getProperty("${summer.web.favicon-path:/favicon.ico}");
+        if(!this.resourcePath.endsWith("/")){
+            this.resourcePath+="/";
+        }
+    }
+
+    public void init() throws ServletException{
+        logger.info("init {}.", getClass().getName());
+        // scan @Controller and @RestController
+        for(var def:((ConfigurableApplicationContext) this.applicationContext).findBeanDefinitions(Object.class)){
+            Class<?> beanClass = def.getBeanClass();
+            Object bean = def.getRequiredInstance();
+            Controller controller = beanClass.getAnnotation(Controller.class);
+            RestController restController = beanClass.getAnnotation(RestController.class);
+            if(controller!=null&&restController!=null){
+                throw new ServletException("Found both @Controller and @RestController on class: "+beanClass.getName());
+            }
+            if(controller!=null){
+                addController(false, def.getName(), bean);
+            }
+            if(restController!=null){
+                addController(true, def.getName(), bean);
+            }
+        }
     }
 
     @Override
@@ -58,12 +84,172 @@ public class DispatcherServlet extends HttpServlet {
         this.applicationContext.close();
     }
 
+    void addController(boolean isRest, String name, Object instance) throws ServletException {
+        logger.info("add {} controller '{}':{}", isRest?"REST":"MVC", name, instance.getClass().getName());
+        addMethod(isRest, name, instance, instance.getClass());
+    }
+
+    void addMethod(boolean isRest, String name, Object instance, Class<?> type) throws ServletException {
+        for (Method m : type.getDeclaredMethods()) {
+            GetMapping get = m.getAnnotation(GetMapping.class);
+            if(get!=null){
+                checkMethod(m);
+                this.getDispatchers.add(new Dispatcher("GET", isRest, instance, m, get.value()));
+            }
+            PostMapping post = m.getAnnotation(PostMapping.class);
+            if(post!=null){
+                checkMethod(m);
+                this.postDispatchers.add(new Dispatcher("POST", isRest, instance, m, post.value()));
+            }
+        }
+        Class<?> superclass = type.getSuperclass();
+        if(superclass != null){
+            addMethod(isRest, name, instance, superclass);
+        }
+    }
+
+    void checkMethod(Method m) throws ServletException {
+        int mod = m.getModifiers();
+        if(Modifier.isStatic(mod)){
+            throw new ServletException("Cannot do URL mapping to static method: "+m);
+        }
+        m.setAccessible(true);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("{} {}", req.getMethod(), req.getRequestURI());
-        PrintWriter pw = resp.getWriter();
-        pw.write("<h1>Hello, world!</h1>");
-        pw.flush();
+        String url = req.getRequestURI();
+        if (url.equals(this.faviconPath) || url.startsWith(this.resourcePath)) {
+            doResource(url, req, resp);
+        } else {
+            doService(req, resp, this.getDispatchers);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doService(req, resp, this.postDispatchers);
+    }
+
+    void doService(HttpServletRequest req, HttpServletResponse resp, List<Dispatcher> dispatchers) throws ServletException, IOException {
+        String url = req.getRequestURI();
+        try {
+            doService(url, req, resp, dispatchers);
+        } catch (ErrorResponseException e) {
+            logger.warn("process request failed with status {} : {}", e.statusCode, url, e);
+            if (!resp.isCommitted()) {
+                resp.resetBuffer();
+                resp.sendError(e.statusCode);
+            }
+        } catch (RuntimeException | ServletException | IOException e) {
+            logger.warn("process request failed: {}", url, e);
+            throw e;
+        } catch (Exception e) {
+            logger.warn("process request failed: {}", url, e);
+            throw new NestedRuntimeException(e);
+        }
+    }
+
+    void doService(String url, HttpServletRequest req, HttpServletResponse resp, List<Dispatcher> dispatchers) throws Exception {
+        for (Dispatcher dispatcher : dispatchers) {
+            Result result = dispatcher.process(url, req, resp);
+            if (result.processed()) {
+                Object r = result.returnObject();
+                if (dispatcher.isRest) {
+                    // send rest response:
+                    if (!resp.isCommitted()) {
+                        resp.setContentType("application/json");
+                    }
+                    if (dispatcher.isResponseBody) {
+                        if (r instanceof String s) {
+                            // send as response body:
+                            PrintWriter pw = resp.getWriter();
+                            pw.write(s);
+                            pw.flush();
+                        } else if (r instanceof byte[] data) {
+                            // send as response body:
+                            ServletOutputStream output = resp.getOutputStream();
+                            output.write(data);
+                            output.flush();
+                        } else {
+                            // error:
+                            throw new ServletException("Unable to process REST result when handle url: " + url);
+                        }
+                    } else if (!dispatcher.isVoid) {
+                        PrintWriter pw = resp.getWriter();
+                        JsonUtils.writeJson(pw, r);
+                        pw.flush();
+                    }
+                } else {
+                    // process MVC:
+                    if (!resp.isCommitted()) {
+                        resp.setContentType("text/html");
+                    }
+                    if (r instanceof String s) {
+                        if (dispatcher.isResponseBody) {
+                            // send as response body:
+                            PrintWriter pw = resp.getWriter();
+                            pw.write(s);
+                            pw.flush();
+                        } else if (s.startsWith("redirect:")) {
+                            // send redirect:
+                            resp.sendRedirect(s.substring(9));
+                        } else {
+                            // error:
+                            throw new ServletException("Unable to process String result when handle url: " + url);
+                        }
+                    } else if (r instanceof byte[] data) {
+                        if (dispatcher.isResponseBody) {
+                            // send as response body:
+                            ServletOutputStream output = resp.getOutputStream();
+                            output.write(data);
+                            output.flush();
+                        } else {
+                            // error:
+                            throw new ServletException("Unable to process byte[] result when handle url: " + url);
+                        }
+                    } else if (r instanceof ModelAndView mv) {
+                        String view = mv.getViewName();
+                        if (view.startsWith("redirect:")) {
+                            // send redirect:
+                            resp.sendRedirect(view.substring(9));
+                        } else {
+                            this.viewResolver.render(view, mv.getModel(), req, resp);
+                        }
+                    } else if (!dispatcher.isVoid && r != null) {
+                        // error:
+                        throw new ServletException("Unable to process " + r.getClass().getName() + " result when handle url: " + url);
+                    }
+                }
+                return;
+            }
+        }
+        // not found:
+        resp.sendError(404, "Not Found");
+    }
+
+    void doResource(String url, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ServletContext ctx = req.getServletContext();
+        try (InputStream input = ctx.getResourceAsStream(url)) {
+            if (input == null) {
+                resp.sendError(404, "Not Found");
+            } else {
+                // guess content type:
+                String file = url;
+                int n = url.lastIndexOf('/');
+                if (n >= 0) {
+                    file = url.substring(n + 1);
+                }
+                String mime = ctx.getMimeType(file);
+                if (mime == null) {
+                    mime = "application/octet-stream";
+                }
+                resp.setContentType(mime);
+                ServletOutputStream output = resp.getOutputStream();
+                input.transferTo(output);
+                output.flush();
+            }
+        }
     }
 
     static class Dispatcher {
@@ -138,7 +324,7 @@ public class DispatcherServlet extends HttpServlet {
                         }
                     };
                 }
-                Object result=null;
+                Object result;
                 try{
                      result = this.handlerMethod.invoke(this.controller, arguments);
                 }catch (InvocationTargetException e){
